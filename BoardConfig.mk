d412edc21c4151fe83981bed8c4ab826bec510f5
alexenferman <alextud0132@gmail.com>
2021-01-03 22:53:46 -0500

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,69 @@
+# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2021 Alexenferman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from common D2
+-include device/samsung/d2-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/samsung/d2att/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2att
+
+# Kernel
+#TARGET_KERNEL_SOURCE		:= kernel/samsung/d2
+#TARGET_KERNEL_CONFIG		:= cyanogen_d2_defconfig
+TARGET_PREBUILT_KERNEL      := device/samsung/d2att/kernel
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_BASE           := 0x80200000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+BOARD_KERNEL_PAGESIZE       := 2048
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+#TWRP
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p15"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "discard,noauto_da_alloc,journal_async_commit"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
